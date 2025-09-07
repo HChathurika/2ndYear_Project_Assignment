39a3393c8c3a343d118256f2ba572ec704302c35
Chathurika <silvachathurtika@outlook.com>
2025-09-07 13:18:44 +0300

    Simulation

--- Assignment_1_2/src/Task3/TestEventSimulation.java
+++ Assignment_1_2/src/Task3/TestEventSimulation.java
@@ -1,4 +1,25 @@
 package Task3;
 
-public class TestEvnSimulation {
+
+public class TestEventSimulation {
+    public static void main(String[] args) {
+        EventList eventList = new EventList();
+
+        // Add some events
+        eventList.addEvent(new Event(5.0, EventType.ARRIVAL));
+        eventList.addEvent(new Event(2.0, EventType.EXIT));
+        eventList.addEvent(new Event(8.0, EventType.ARRIVAL));
+        eventList.addEvent(new Event(3.0, EventType.ARRIVAL));
+
+        System.out.println("All events in order:");
+        eventList.printEvents();
+
+        System.out.println("\nNext event to process:");
+        Event next = eventList.getNextEvent();
+        System.out.println(next);
+
+        System.out.println("\nRemaining events in order:");
+        eventList.printEvents();
+    }
 }
+
