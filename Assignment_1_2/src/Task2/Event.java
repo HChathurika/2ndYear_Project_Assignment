fe3e1bda1e134d838c00a956246244f6b2ae6e5d
Chathurika <silvachathurtika@outlook.com>
2025-09-07 13:08:07 +0300

    Done

--- Assignment_1_2/src/Task2/Event.java
+++ Assignment_1_2/src/Task2/Event.java
@@ -1,4 +1,26 @@
 package Task2;
 
-public class Event {
+
+public class Event implements Comparable<Event> {
+    private double time;  // when the event occurs
+
+    public Event(double time) {
+        this.time = time;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public int compareTo(Event other) {
+        // smaller time = earlier event
+        return Double.compare(this.time, other.time);
+    }
+
+    @Override
+    public String toString() {
+        return "Event at time " + time;
+    }
 }
+
